62ebdb76a32ef0c3e315f64089e70865754809be
Rafael Santana <aguiar.rafael003@gmail.com>
2026-03-12 16:21:37 -0300

    feat: minha primeira apresentacao em C

---
+++ apresentacao.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    printf("Nome: Rafael Santana \n");
+    printf("Aluno do 1o semestre de Ciencia da Computacao\n");
+    printf("Objetivo: aprender a programar em C\n");
+    return 0;
+}
